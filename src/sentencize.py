36c2cfe57aaa0673e17a9074e39e17820692c84f
kimata.20038 <kimata.20038@srv10.tti-coin.toyota-ti.ac.jp>
2022-10-10 17:36:06 +0900

    test

--- src/sentencize.py
+++ src/sentencize.py
@@ -1,1 +1,21 @@
-#hello world
+#関数の作成
+#ファイルと列番号を受け取る。
+import sys
+
+
+def sentencize(file_name,row):
+    with open(file_name) as f: 
+        sentence = f.read() #ファイルを文字列として読み込み
+    sentence = sentence.replace("\n"," ") #文字列の改行を空白に置換する
+    sentence = sentence.split("  ") #"  "で区切り、リストにする。
+    for sen in sentence[1:-1]:
+        sen = sen.split(" ")
+        sentence = [ c for num,c in enumerate(sen) if num % 4 == row]
+        sentence = " ".join(sentence)
+        #if sentence == "-DOCSTART-" or sentence == "-X-" or sentence == "O":
+        #    sentence 
+        print(sentence)
+
+x = sys.argv[1]
+y = sys.argv[2]
+sentencize(str(x),int(y))
